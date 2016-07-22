f7ea692b7801320b4bc9611549974156ebec0ef5
Eric Westcott <eric@ericwestcott.com>
2016-07-22 11:34:42 -0400

    jadencase solution

--- test.js
+++ test.js
@@ -2,9 +2,19 @@ var str = "How can mirrors be real if our eyes aren't real";
 
 String.prototype.toJadenCase = function () {
   //...
-  function toJadenCase() {
-    console.log('Hello');
+  var capIt = this.split(" ");
+
+  var array = [];
+
+  console.log(capIt);
+
+  for (var i = 0; i < capIt.length; i++){
+    var string = capIt[i].charAt(0).toUpperCase() + capIt[i].substring(1);
+    array.push(string);
   }
 
-  console.log(this.charAt(0).toUpperCase() + this.slice(1));
+  console.log(array.join(' '));
+
 };
+
+str.toJadenCase();
