15cf60ba77b3084d881caaa287abd323da922cdf
Eric Westcott <eric@ericwestcott.com>
2016-07-28 14:27:02 -0400

    finding consecutive integers

---
+++ test.js
@@ -0,0 +1,12 @@
+/**
+ * Given the number of consecutive integers and the total of the integers,
+ * return the consecutive integer at the requested position.
+ *
+ * @param {int} x number of consecutive integers
+ * @param {int} y sum of consecutive integers
+ * @param {int} n position of requested integer
+ * @return {int} consecutive integer at requested position
+ */
+function position(x, y, n) {
+  // TODO: return consecutive integer at requested position
+}
