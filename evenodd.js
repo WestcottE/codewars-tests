67cc87f08ffd9d8cdd9e302e93d5944203fe1c5a
Eric <eric@ericwestcott.com>
2016-08-15 13:18:28 -0400

    commit

---
+++ evenodd.js
@@ -0,0 +1,13 @@
+function even_or_odd(number) {
+  // ...
+  console.log(number % 2);
+  if (number % 2) {
+    console.log("Even");
+  }
+  else {
+    console.log("Odd");
+  }
+}
+
+even_or_odd(2);
+even_or_odd(3);
---
+++ test2.js
@@ -0,0 +1,7 @@
+function titleCase(title, minorWords) {
+  var lowTitle = title.ToLowerCase();
+
+  console.log(lowTitle);
+}
+
+titleCase('a clash of KINGS', 'a an the of');
